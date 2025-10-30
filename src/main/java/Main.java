4b37c94b92df278d81347cca33c9d19c953b0596
Maiia Borinos <maya.borinos@gmail.com>
2025-10-30 22:46:53 +0400

    Проверка корректности введенного пути и подсчет файлов

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,24 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
+        int cout = 1;
+        while(1==1) {
+            System.out.println("Введите путь к файлу и нажмите <Enter>: ");
+            String path = new Scanner(System.in).nextLine();
+
+            File file = new File(path);
+            boolean fileExists = file.exists(); // проверка существует ли файл
+            boolean isDirectory = file.isDirectory(); // проверка, что путь именно к файлу
+
+            if(fileExists == false || isDirectory == false){
+                System.out.println("Указанный файл не существует или указанный путь является путём к папке, а не к файлу" + "\n");
+                continue;
+            }
+            System.out.println("Путь указан верно");
+            System.out.println("Это файл номер " + cout++ + "\n");
+        }
     }
 }
+

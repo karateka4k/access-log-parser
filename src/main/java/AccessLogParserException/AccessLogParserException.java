8941477f123e1b9b7efae38bea695bf168bdedff
Maiia Borinos <maya.borinos@gmail.com>
2026-02-10 22:38:00 +0400

    Задание 9.1. Обработка исключений

---
+++ src/main/java/AccessLogParserException/AccessLogParserException.java
@@ -0,0 +1,7 @@
+package AccessLogParserException;
+
+public class AccessLogParserException extends Exception {
+    public AccessLogParserException(String message) {
+        super(message);
+    }
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,20 @@
+import AccessLogParserException.AccessLogParserException;
+
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         int cout = 1;
+        Scanner scanner = new Scanner(System.in);
+        String path;
+
+
         while(1==1) {
             System.out.println("Введите путь к файлу и нажмите <Enter>: ");
-            String path = new Scanner(System.in).nextLine();
+            path = scanner.nextLine();
 
             File file = new File(path);
             boolean fileExists = file.exists(); // проверка существует ли файл
@@ -19,7 +27,43 @@ public class Main {
 
             System.out.println("Путь указан верно");
             System.out.println("Это файл номер " + cout++ + "\n");
+
+            scanner.close();
+            break;
         }
+
+        try {
+            int countLines = 0;
+            int minLineLength = Integer.MAX_VALUE;
+            int maxLineLength = Integer.MIN_VALUE;
+
+            FileReader fileReader = new FileReader(path);
+            BufferedReader reader =
+                    new BufferedReader(fileReader);
+            String line;
+            while ((line = reader.readLine()) != null) {
+                int length = line.length();
+                validateLineLength(length);
+
+                countLines++;
+                if (length < minLineLength) minLineLength = length;
+                if (length > maxLineLength) maxLineLength = length;
+            }
+
+            System.out.printf("Общее количество строк: %s%n", countLines);
+            System.out.printf("Минимальная длина строки: %s символа%n", minLineLength);
+            System.out.printf("Максимальнмя длина строки: %s символа%n", maxLineLength);
+
+        } catch  (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static void validateLineLength(int lineLength) throws AccessLogParserException {
+        final int LINE_LENGHT_LIMIT = 1024;
+
+        if (lineLength > LINE_LENGHT_LIMIT) throw new
+                AccessLogParserException(String.format("Длина строки больше %s символов. Длина строки: %s символа", LINE_LENGHT_LIMIT, lineLength));
     }
 }
 
